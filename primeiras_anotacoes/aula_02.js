34708d44a3410a4675eac420ce288383d44afda2
Matheus Roks <matheusroksgomesrodrigues@gmail.com>
2025-07-30 16:34:17 -0400

    Create aula_02.js

---
+++ primeiras_anotacoes/aula_02.js
@@ -0,0 +1,22 @@
+/* Um pouco sobre os tipos de dado
+segue o bom e velho principio da lógica de programação.
+temos o tipo texto, caracter, numero, boolean, objeto, null e undefined.
+undefined seria um dado não definido, ou seja, variável não declarada.
+null é vazio, o nosso none
+caracter se refere a simbolos.
+
+
+COERÇÃO
+
+a coerção é feita ainda de forma mais simples, no aspecto da variável.
+os valores da variável são bem mutáveis, então se voce desejar pode 
+simplesmente fazer o seguinte.
+
+var name = "João"
+name = 1.235
+
+isso não vai causar nenhuma quebra no código (mas é aquilo, variáveis
+devem ser bem declaradas para evitar comentários denecessários e devem conter
+exatamente o que o nome diz conter.)
+*/
+
