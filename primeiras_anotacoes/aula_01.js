20f53c974377194dc2af0fd3d86b21021aef89d9
Matheus Roks <matheusroksgomesrodrigues@gmail.com>
2025-07-30 15:46:00 -0400

    Create aula_01.js

---
+++ primeiras_anotacoes/aula_01.js
@@ -0,0 +1,9 @@
+// temos algumas semelhanças entre o  javascript e outras linguagens 
+
+// a forma que o javascript entende as variáveis são semelhantes ao
+// python, com seu dinamismo em nao ser necessário determinar o tipo
+// antes de atribuir o elemento, coisa que não ocorre no Java.
+
+//sim, apesar de Java e JavaScript começarem com as mesmas 4 letras, isso
+// não as torna semelhantes ou denota alguma relação, afinal
+//amar e amargo começam com as mesmas 4 letras mas são distintos
