e1d9aaa812e9926463cac0b8aeaf570c5c688ab1
Matheus Roks <matheusroksgomesrodrigues@gmail.com>
2025-07-30 15:56:55 -0400

    Update aula_01.js

--- primeiras_anotacoes/aula_01.js
+++ primeiras_anotacoes/aula_01.js
@@ -7,3 +7,30 @@
 //sim, apesar de Java e JavaScript começarem com as mesmas 4 letras, isso
 // não as torna semelhantes ou denota alguma relação, afinal
 //amar e amargo começam com as mesmas 4 letras mas são distintos
+
+var nomeUsuario = "Matheus";
+//essa seria uma das formas de declarar a variável.
+/* quando usamos 'var' para definir a variável, podemos ou não passar o valor
+e sua principal caracteristica é que se torna válida para todo o código
+ */
+
+let age = 23;
+/* a variável 'let' já se caracteriza por ser uma variável de escopo local, ou
+seja, ela não é valida para todo o código, mas para um determinado bloco de 
+código
+ */
+
+const PI = 3.1415;
+/* A constante segue o mesmo principio de uma constante em java.
+não pode ser alterada, serve apenas para ser usada como uma constante.
+por padrão se é escrito em letras maiusculas
+*/
+
+/* durante minhas leituras ainda não ficou claro a forma correta de se 
+declara uma variável em JS, quais são as convenções.
+no mdn fala sobre iniciar com letra, $ ou _, seguido de letras e numeros
+(se desejar). já o livro que estou lendo JavaScript do Kelvin, apresenta
+as variáveis da forma que nomeei nome do usuário. nomeUsuario ou 
+nomeDoUsuario. posteriormente posso vir aqui e adicionar uma nova anotação
+conforme minha duvida seja saciada
+ */
