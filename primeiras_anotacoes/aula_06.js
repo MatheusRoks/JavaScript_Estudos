64264d2aaa7a12571f2b9000d8d4e244042de0ed
Matheus Roks <matheusroksgomesrodrigues@gmail.com>
2025-07-31 14:12:50 -0400

    Create aula_06.js

---
+++ primeiras_anotacoes/aula_06.js
@@ -0,0 +1,20 @@
+/*
+os operadores matemáticos são os mesmos, funcionam da mesma forma.
++ soma 
+- subtração 
+* multiplicação
+/ divisão real
+% modulo
+** exponenciação
++= variavel recebe uma soma dela mesma + algo
+-= variavel - alguma coisa
+*=
+/=
+%=
+**=
+além desses elementos, temos o (a variavel será representada por x)
+x++ soma um
+x-- subtrai um
+--x subtrai um
+++x soma um
+*/
